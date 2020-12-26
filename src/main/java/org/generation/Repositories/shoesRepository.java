66c9809f4eb15816fdb8ee405a442a2131ab8a15
Leonardo <you@example.com>
2020-12-26 18:18:51 -0300

    Update

--- src/main/java/org/generation/Repositories/shoesRepository.java
+++ src/main/java/org/generation/Repositories/shoesRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.generation.Models.shoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface shoesRepository extends JpaRepository<shoes, Long> {
     public List <shoes> findAllByNameContainingIgnoreCase (String shoeName);
     public List <shoes> findAllBySize (int size);
